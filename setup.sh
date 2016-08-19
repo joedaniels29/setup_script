4b7bcefb6997162438e9e9569e39f4bcb1609994
Joseph Daniels <joedaniels29@me.com>
2016-08-18 22:52:36 -0400

    fix for gh?

--- setup.sh
+++ setup.sh
@@ -317,8 +317,8 @@ npm install -g ember-cli
 
 
 curl https://gist.githubusercontent.com/joedaniels29/f9fb30554649f68d2c973f8d4e98b68b/raw > .tmux.conf
-curl https://gist.github.com/4d30854664085d75d78b71b735d26647/raw > ~/Library/Keyboard\ Layouts/simple.keylayout
-curl https://gist.github.com/4d30854664085d75d78b71b735d26647/raw > ~/Library/Preferences/com.divisiblebyzero.Spectacle.plist
+curl https://gist.github.com/joedaniels29/4d30854664085d75d78b71b735d26647/raw > ~/Library/Keyboard\ Layouts/simple.keylayout
+curl https://gist.github.com/joedaniels29/4d30854664085d75d78b71b735d26647/raw > ~/Library/Preferences/com.divisiblebyzero.Spectacle.plist
 wget https://www.dropbox.com/s/63gjkdrck5wiqy3/com.divisiblebyzero.Spectacle.plist?dl=1 -O ~/Library/Preferences/com.divisiblebyzero.Spectacle.plist
 wget https://gist.github.com/joedaniels29/647388ca7a36d19762614dbd454ff0e8/raw -O ~/Library/Application\ Support/Karabiner/private.xml
-wget https://gist.github.com/909a18f542196800eeb5145b7f3a68db/raw -O /Users/Joe/Library/Developer/Xcode/UserData/KeyBindings/Joes.idekeybindings
+wget https://gist.github.com/joedaniels29/909a18f542196800eeb5145b7f3a68db/raw -O /Users/Joe/Library/Developer/Xcode/UserData/KeyBindings/Joes.idekeybindings
