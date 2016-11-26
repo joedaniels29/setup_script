a06769dfa9f330fb05dce990354b04f071b365c7
Joseph Daniels <joedaniels29@me.com>
2016-11-25 19:58:20 -0500

    adds appcode.3

--- user_environment.zsh
+++ user_environment.zsh
@@ -13,15 +13,15 @@ for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -fs "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 
-git clone  https://github.com/github.com/joedaniels29/zsh_scripts.git "~/.zsh_scripts";
-git clone  https://github.com/joedaniels29/.tmuxinator.git "~/.tmuxinator";
+git clone  https://github.com/github.com/joedaniels29/zsh_scripts.git ~/.zsh_scripts;
+git clone  https://github.com/joedaniels29/.tmuxinator.git ~/.tmuxinator;
 source ~/.zshrc
 for f (FiraCode-Bold.ttf FiraCode-Light.ttf FiraCode-Medium.ttf FiraCode-Regular.ttf FiraCode-Retina.ttf); do
     wget https://github.com/tonsky/FiraCode/raw/master/distr/ttf/$f -O /Library/Fonts/$f
 done
 # Preferences
 cd ~/Library/Preferences
-for f (IntelliJIdea2016.2 AppCode2016.2); do
+for f (IntelliJIdea2016.2 AppCode2016.2 AppCode2016.3); do
     git clone https://github.com/joedaniels29/$f
 done
 cd ~
